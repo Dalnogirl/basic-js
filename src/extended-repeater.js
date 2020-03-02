22d7a93e1cb2865fce425a3b68ea3fa7a58d1f5d
Hleb <velikiizevs@gmail.com>
2020-03-02 21:16:05 +0300

    feat: extended-repeater solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,12 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-};
-  
+module.exports = function repeater(str, options) {
+    if (options.hasOwnProperty('addition')) {
+        options.addition += '';
+        if (options.hasOwnProperty('additionRepeatTimes') || options.hasOwnProperty('additionSeparator')) {
+            options.addition = repeater(options.addition, { repeatTimes: options.additionRepeatTimes, separator: options.additionSeparator });
+        }
+        str += options.addition;
+    }
+    const repeatTimes = options.repeatTimes ? options.repeatTimes : 1;
+    const separator = options.hasOwnProperty('separator') ? options.separator : '+';
+    return new Array(repeatTimes).fill(str).join(separator);
+}
